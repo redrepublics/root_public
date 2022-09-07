8d87f24f6d137e0cd988478aa4b57b21c5e322ad
Жеребьев Владислав <vjerebiov@SystechPRG.local>
2022-09-07 10:22:23 +0300

    «backup_tsql»

---
+++ tsql/backupcopy52.sql
@@ -0,0 +1,2 @@
+BACKUP DATABASE [SRW_688] TO  DISK = N'C:\ASPO_7\Database\31.bak' WITH NOFORMAT, NOINIT,  NAME = N'SRW_688-Полная База данных Резервное копирование', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+GO
